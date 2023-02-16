874dfc33aaa50537e96365a68546d3209c18617e
Igbe Ajaga <ajagaigbe144@gmail.com>
2023-02-16 15:18:54 -0800

    printing a string in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * returnimg 0 always successful
+ */
+int main(void)
+{
+  puts("\"programming is like building a multilingual puzzle");
+  return(0);
+}
