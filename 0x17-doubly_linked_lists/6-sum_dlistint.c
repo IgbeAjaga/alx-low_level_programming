183237edbf70e6b46988a5724b869b5b3e4382f8
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-18 12:09:01 -0700

    a function that returns the sum of all the data (n) of a dlistint_t linked list.

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * sum_dlistint - a function that returns the sum of all the data of a linked list
+ * @head: first node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *current = head;
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
