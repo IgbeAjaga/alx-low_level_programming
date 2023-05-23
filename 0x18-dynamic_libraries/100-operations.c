e2f5b927c69dcccfdf3c304f4739c10f8ba6398d
IgbeAjaga <ajagaigbe144@gmail.com>
2023-05-23 05:11:44 -0700

    a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * add - Entry point addition
+ * @a: integer
+ * @b: integer
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Entry point subtraction
+ * @a: integer
+ * @b: integer
+ * Return: Always 0 (Success)
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Entry point multiplication
+ * @a: integer
+ * @b: integer
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Entry point division
+ * @a: integer
+ * @b: integer
+ * Return: Always 0 (Success)
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Entry point modulus
+ * @a: integer
+ * @b: integer
+ * Return: Always 0 (Success)
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
